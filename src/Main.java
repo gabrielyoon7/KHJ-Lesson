dc83f065c29692a7875dfa2e8a539f022ffacfbe
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-28 15:45:33 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int num = 1;
+        String name = "김휘재";
+        double doubleValue = 1.2;
+        boolean isValid = false;
+        System.out.println("출력하고싶은 말");
     }
 }
